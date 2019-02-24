653106dbacd65964e12f7ae1c87b4a50bfbe42b2
SJYbetter <sjyhyn@gmail.com>
2019-02-23 22:59:24 -0500

    Update KClosetPoints.java

--- OA/src/KClosetPoints.java
+++ OA/src/KClosetPoints.java
@@ -2,7 +2,7 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /*
- * Input: points = [[4,6],[4,7],[4,4],[2,5],[1,1]], origin = [0, 0], k = 3 
+ * Input: points = [[4,6],[4,7],[4,4],[2,5],[1,1]], origin = [0, 0], k = 3
    Output: [[1,1],[2,5],[4,4]]
  */
 public class KClosetPoints {
@@ -12,18 +12,18 @@ public class KClosetPoints {
 		    Point() { x = 0; y = 0; }
 		    Point(int a, int b) { x = a; y = b; }
 		  }
-	
+
     private Point global_origin = null;
-    
+
     public Point[] kClosest(Point[] points, Point origin, int k) {
         // write your code here
         global_origin = origin;
-        
+
         if (points == null || points.length == 0) return points;
-        
+
         //Map<Point, Integer> map = new HashMap<>();
         Point[] ans = new Point[k];
-        //use a prorityqueue to 
+        //use a prorityqueue to
         PriorityQueue<Point> pq = new PriorityQueue<Point> (k, new Comparator<Point> () {
             @Override
             public int compare(Point a, Point b) {
@@ -35,24 +35,77 @@ public class KClosetPoints {
                 return diff;
             }
         });
-        
+
         for (int i = 0; i < points.length; i++) {
             pq.offer(points[i]);
             //check size
             if (pq.size() > k)
                 pq.poll();
         }
-        
+
        // k = pq.size();
-        //Point[] ret = new Point[k];  
+        //Point[] ret = new Point[k];
         while ( !pq.isEmpty())
             ans[--k] = pq.poll();
         return ans;
     }
-        
- 
+
+
      private int getDistance(Point a, Point b) {
         return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
     }
 
+}
+
+
+
+/*
+Input: points = [[1,3],[-2,2]], K = 1
+Output: [[-2,2]]
+Explanation:
+The distance between (1, 3) and the origin is sqrt(10).
+The distance between (-2, 2) and the origin is sqrt(8).
+Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+We only want the closest K = 1 points from the origin, so the answer is just [[-2,2]].
+*/
+public class KClosetPoints1{
+	public int[][] kClosest(int[][] points, int K) {
+        int m = points.length, n = points[0].length;
+
+        Map<int[], Integer> map = new HashMap<>();
+        int[][] ans = new int[K][2];
+
+        for (int i = 0; i < m; i++){
+            int[] onePoint = points[i];
+            map.put(onePoint, getDis(onePoint));
+        }
+
+        for (int i = 0; i < K; i++){
+            ans[i] = Collections.min(map.entrySet(), Map.Entry.comparingByValue()).getKey();
+            map.remove(ans[i]);
+        }
+        return ans;
+    }
+
+    private int getDis(int[] point){
+        return point[0]*point[0] + point[1]*point[1];
+    }
+}
+
+public class KClosetPoints2{
+	public int[][] kCloset(int[][] points, int k){
+		PriorityQueue<int[]> pq = new PriorityQueue<>((p1, p2) -> (p2[0]*p2[0] + p2[1]*p2[1] - p1[0]*p1[0] - p1[1]*p1[1]));
+		for (int i = 0; i < points.length; i++){
+			pq.offer(points[i]);
+			if (pq.size() > k){
+				pq.poll();
+			}
+		}
+
+		int[][] ans = new int[k][2];
+		for (int i = k-1; i >= 0; i--){
+			ans[i] = pq.peek();
+			pq.poll();
+		}
+	}
 }
