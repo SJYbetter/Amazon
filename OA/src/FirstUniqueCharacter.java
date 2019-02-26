65e7d1ff9cd6b9e1ab32050f319d6b6267bd3215
SJYbetter <sjyhyn@gmail.com>
2019-02-25 21:22:40 -0500

    update twosum & firstuniquechar

--- OA/src/FirstUniqueCharacter.java
+++ OA/src/FirstUniqueCharacter.java
@@ -1,4 +1,5 @@
 public class FirstUniqueCharacter{
+    //返回index
     public int firstUnique(Stirng s){
       if (s == null || "".equals(s)) return -1;
 
@@ -14,4 +15,22 @@ public class FirstUniqueCharacter{
 
       return -1;
     }
+
+
+
+
+    //返回char
+    public char firstUniqChar(String str) {
+    // Write your code here
+    if (str == null || "".equals(str)) return (char) -1;
+
+    char[] check = new char[26];
+    for (int i = 0; i < str.length(); i++){
+        check[str.charAt(i) - 'a'] += 1;
+    }
+    for (int i = 0; i < str.length(); i++){
+        if (check[str.charAt(i) - 'a'] == 1) return str.charAt(i);
+    }
+    return (char) -1;
+}
 }
--- OA/src/TwoSumClosestToTarget.java
+++ OA/src/TwoSumClosestToTarget.java
@@ -19,4 +19,28 @@ public class TwoSumCloset{
         }
         return DIF;
     }
+
+
+    //返回有多少组pair相加大于target的
+    public int twoSumGreaterTarget(int[] nums, int target){
+        if (nums == null || nums.length == 0) return -1;
+        int count = 0;
+
+        for (int i = 0; i < nums.length-1; i++){
+            for (int j = i+1; j < nums.length; j++){
+                if (nums[i] + nums[j] > target){
+                    count += (len - j);
+                    break;// end the second for loop
+                }
+            }
+        }
+        return count;
+    }
+
+
+
+
+
+
+
 }
