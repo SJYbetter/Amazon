3c11b539bccd3ee83cc591e63e4550616b858051
SJYbetter <sjyhyn@gmail.com>
2019-03-11 10:10:14 -0400

    Update MinDepthOfBinaryTree.java

--- OA/src/MinDepthOfBinaryTree.java
+++ OA/src/MinDepthOfBinaryTree.java
@@ -14,4 +14,11 @@ public class Solution{
         int right = miniDepth(root.right);
         return (left == 0 || right == 0) ? left + right + 1 : Math.min(left, right) + 1;
     }
+
+    public int maxDepth(TreeNode root){
+        if (root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right)+1;
+    }
 }
