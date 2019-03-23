79759f53c8d2de451a88fc7681edf089bcdcaf78
SJYbetter <sjyhyn@gmail.com>
2019-03-23 11:55:45 -0400

    add .

---
+++ OA/src/MinStack.java
@@ -0,0 +1,48 @@
+public class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minstack;
+    public MinStack() {
+        // do intialization if necessary
+        stack = new Stack<>();
+        minstack = new Stack<>();
+    }
+
+    /*
+     * @param number: An integer
+     * @return: nothing
+     */
+    public void push(int number) {
+        // write your code here
+        helper(number);
+    }
+
+    private void helper(int x){
+        int tempMin = minstack.isEmpty() ? Integer.MAX_VALUE : minstack.peek();
+        if (x < tempMin){
+            tempMin  = x;
+        }
+        stack.push(x);
+        minstack.push(tempMin);
+    }
+
+
+    /*
+     * @return: An integer
+     */
+    public int pop() {
+        // write your code here
+        minstack.pop();
+        return stack.pop();
+    }
+
+    /*
+     * @return: An integer
+     */
+    public int min() {
+        // write your code here
+        return minstack.peek();
+
+        //find mini
+
+    }
+}
---
+++ OA/src/SlidingWindowMaximum.java
@@ -0,0 +1,8 @@
+public class Solution{
+    public int[] slidingWindowMaximum(int[] nums, int k){
+        if (nums == null) return new int[]{};
+        for (int i = 0; i < nums.length - k; i++){
+            
+        }
+    }
+}
