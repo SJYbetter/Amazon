c75a94c0afed28d01d8ad192a00774b9738390d5
SJYbetter <sjyhyn@gmail.com>
2019-03-25 00:03:38 -0400

    Update FindMedianfromDataStream.java

--- OA/src/FindMedianfromDataStream.java
+++ OA/src/FindMedianfromDataStream.java
@@ -13,21 +13,49 @@ double findMedian() - Return the median of all elements so far.
 
 
 class MedianFinder {
+    private PriorityQueue<Integer> minHeap, maxHeap;
+    private int size;
+    private int median;
 
     /** initialize your data structure here. */
     public MedianFinder() {
-        int minSize = minHeap.size();
-        int maxSize = maxHeap.size();
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((c1,c2) -> c2-c1);
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>((c1,c2) -> c2-c1);
+        size = 0;
+        median = 0;
     }
 
     public void addNum(int num) {
+        if (size == 0){
+            maxHeap.offer(num);
+            size ++;
+            median = num;
+        }
+
+        if (size != 0){
+            if (num <= median){
+                maxHeap.offer(num);
+            }else{
+                minHeap.offer(num);
+            }
+
+            if (maxHeap.size() > minHeap.size()){
+                minHeap.offer(median);
+                median = maxHeap.poll();
+            }else if (minHeap.size() - maxHeap.size() > 1){
+                maxHeap.offer(median);
+                median = minHeap.poll();
+            }
+            size ++;
+        }
 
     }
 
     public double findMedian() {
-        if 
+        if (size == 0) return 0;
+        if (size % 2 != 0) return median;
+        return (double) (median + maxHeap.peek())/2;
+
 
     }
 }
