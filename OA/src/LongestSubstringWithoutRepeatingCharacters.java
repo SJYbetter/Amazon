e9695f5d07926e9fbe6527e16abbca53e3415921
SJYbetter <sjyhyn@gmail.com>
2019-03-25 21:51:41 -0400

    update

--- OA/src/LongestSubstringWithoutRepeatingCharacters.java
+++ OA/src/LongestSubstringWithoutRepeatingCharacters.java
@@ -26,4 +26,24 @@ class Solution{
          }
          return ans;
     }
+
+    public int longestSubstring1(String s){
+        if (s == null || "".equals(s)) return 0;
+
+        int i = 0, j = 0, max = 0;
+        Set<Character> set = new HashSet<>();
+
+        while (j < s.length()){
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j));
+                j++;
+                max = Math.max(max, set.size());
+            }else{
+                set.remove(s.charAt(i));
+                i ++;
+            }
+
+        }
+        return max;
+    }
 }
