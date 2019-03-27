d825f8835cf1ec92cb0ed1452c1f9f43f7319571
SJYbetter <sjyhyn@gmail.com>
2019-03-26 23:47:15 -0400

    add .

---
+++ OA/src/SortByTwoStacks.java
@@ -0,0 +1,4 @@
+//Create a temporary stack say tmpStack.
+//While input stack is NOT empty do this:
+//Pop an element from input stack call it temp
+//while temporary stack is NOT empty and top of temporary stack is greater than temp, pop from temporary stack and push it to the input stack, push temp in temporary stack, The sorted numbers are in tmpStack.
