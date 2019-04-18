292a4fd3c5280a04be9608af21bcb150da8833f9
SJYbetter <sjyhyn@gmail.com>
2019-04-18 09:40:38 -0400

    up

--- OA/src/ClosestBinarySearchTreeValue.java
+++ OA/src/ClosestBinarySearchTreeValue.java
@@ -2,9 +2,9 @@ class Solution {
 
     public int closestValue(TreeNode root, double target) {
         int closet = root.val;
-        if(root.val > target && root.left != null)
+        if (root.val > target && root.left != null)
             closet = closestValue(root.left, target);
-        else if(root.right != null)
+        else if (root.right != null)
             closet = closestValue(root.right, target);
         return Math.abs(target - root.val) <= Math.abs(target - closet) ? root.val : closet;
     }
